b1910a1ce366b73ba27aa56590ecdf306a9d12ec
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-09-28 17:08:00 +0530

    Added login page

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Button, Form, Grid, Segment, Header, Message } from "semantic-ui-react"
+
+class LoginForm extends React.Component {
+	render() {
+		return (
+			<Grid textAlign='center' style={{ height: "100vh" }} verticalAlign='middle'>
+				<Grid.Column style={{ maxWidth: 450 }}>
+					<Header as='h3' color='orange' textAlign='center'>
+						Hello, there. Login to your account
+					</Header>
+					<Form size='large'>
+						<Segment raised inverted color='orange' secondary>
+							<Form.Input fluid icon='address card' iconPosition='left' placeholder='Email' type='input' />
+							<Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' type='password' />
+							<Button type='submit'>Login</Button>
+							<Button inverted>Sign Up</Button>
+						</Segment>
+						<Message>
+							Forgot your password? <a href='#'>reset</a>
+						</Message>
+					</Form>
+				</Grid.Column>
+			</Grid>
+		)
+	}
+}
+
+export default LoginForm
