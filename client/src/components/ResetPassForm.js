c422f32e97619293934c60e2baf6f3660a19b409
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-09-28 21:04:16 +0530

    Added SignUp and ResetPassword components

---
+++ client/src/components/ResetPassForm.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Button, Form, Grid, Segment, Header, Message } from "semantic-ui-react"
+
+function ResetPassForm() {
+	return <p>hello</p>
+}
+
+export default ResetPassForm
