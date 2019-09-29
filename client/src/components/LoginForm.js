8b35d3403e3fae3f76f06442d5e9b05c8f51b1e4
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-09-29 21:44:08 +0530

    Reverted to old required style and added routing

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,10 +1,41 @@
 import React, { useState } from "react"
 import { Button, Form, Grid, Segment, Header, Message } from "semantic-ui-react"
 import { Link, Redirect } from "react-router-dom"
+import axios from "axios"
 
 function LoginForm() {
 	const [signUp, setSignUp] = useState(false)
+	const [allFilled, setAllFilled] = useState(true)
+	const [correctEmail, setCorrectEmail] = useState(true)
 	document.title = "CollegeDashboard | Login"
+
+	function validateAndSubmit(e) {
+		e.preventDefault()
+		let email = e.target.email.value
+		let pass = e.target.password.value
+		const data = {
+			email: email,
+			password: pass
+		}
+
+		if (!(email.includes("@") && email.includes("."))) {
+			setCorrectEmail(false)
+			return
+		} else setCorrectEmail(true)
+
+		if (!email || !pass) {
+			setAllFilled(false)
+			return
+		} else setAllFilled(true)
+
+		if (correctEmail && allFilled)
+			axios
+				.post("http://localhost:4000/login/submit", {
+					body: data
+				})
+				.then(res => console.log(res.data))
+				.catch(err => console.log(err))
+	}
 
 	if (!signUp) {
 		return (
@@ -13,11 +44,31 @@ function LoginForm() {
 					<Header as='h2' color='orange' textAlign='center'>
 						Hello, there. Login to your account
 					</Header>
-					<Form size='large'>
+					<Form size='large' onSubmit={validateAndSubmit}>
 						<Segment raised inverted color='orange' secondary>
-							<Form.Input fluid icon='address card' iconPosition='left' placeholder='Email' name='email' type='input' />
-							<Message warning header='Could you check something!' list={["That e-mail has been subscribed, but you have not yet clicked the verification link in your e-mail."]} />
-							<Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' name='password' type='password' />
+							<Form.Input
+								fluid
+								icon='address card'
+								iconPosition='left'
+								placeholder='Email'
+								name='email'
+								type='input'
+							/>
+							<Message
+								warning
+								header='Could you check something!'
+								list={[
+									"That e-mail has been subscribed, but you have not yet clicked the verification link in your e-mail."
+								]}
+							/>
+							<Form.Input
+								fluid
+								icon='lock'
+								iconPosition='left'
+								placeholder='Password'
+								name='password'
+								type='password'
+							/>
 							<Button type='submit'>Login</Button>
 							<Button inverted onClick={() => setSignUp(true)}>
 								Sign Up
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,7 +3,6 @@ import { Button, Form, Grid, Segment, Header, Message } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 import axios from "axios"
 
-
 function SignUp() {
 	const [passMatch, setPassMatch] = useState(true)
 	const [correctEmail, setCorrectEmail] = useState(true)
@@ -28,11 +27,15 @@ function SignUp() {
 		if (pass !== repass) setPassMatch(false)
 		else setPassMatch(true)
 
-		if (!email.includes("@") || !email.includes(".")) setCorrectEmail(false)
-		else setCorrectEmail(true)
+		if (!(email.includes("@") && email.includes("."))) {
+			setCorrectEmail(false)
+			return
+		} else setCorrectEmail(true)
 
-		if (!email || !pass || !repass || !name) setAllFilled(false)
-		else setAllFilled(true)
+		if (!email || !pass || !repass || !name) {
+			setAllFilled(false)
+			return
+		} else setAllFilled(true)
 
 		if (passMatch && correctEmail && allFilled)
 			axios
@@ -52,14 +55,47 @@ function SignUp() {
 				<Form error size='large' onSubmit={validateAndSubmit}>
 					<Segment raised inverted color='orange' secondary size='large' textAlign='left'>
 						<Form.Input fluid label='Enter Email' placeholder='Email' name='email' type='input' />
-						{correctEmail === false && <Message error header='Invalid email' content='Check your email address' size='small' />}
+						{correctEmail === false && (
+							<Message
+								error
+								header='Invalid email'
+								content='Check your email address'
+								size='small'
+							/>
+						)}
 						<Form.Input fluid label='Enter Name' placeholder='Name' name='name' type='input' />
 						<Form.Input fluid label='Enter USN' placeholder='USN' name='usn' type='input' />
-						<Form.Input fluid label='Enter Password' placeholder='Password' name='password' type='password' />
-						<Form.Input fluid label='Re-enter Password' placeholder='Password' name='repassword' type='password' />
-						{passMatch === false && <Message error header='Passwords do not match' content='Make sure you re-enter the same password' size='small' />}
+						<Form.Input
+							fluid
+							label='Enter Password'
+							placeholder='Password'
+							name='password'
+							type='password'
+						/>
+						<Form.Input
+							fluid
+							label='Re-enter Password'
+							placeholder='Password'
+							name='repassword'
+							type='password'
+						/>
+						{passMatch === false && (
+							<Message
+								error
+								header='Passwords do not match'
+								content='Make sure you re-enter the same password'
+								size='small'
+							/>
+						)}
 						<Button type='submit'>Sign Up</Button>
-						{allFilled === false && <Message error header='All fields compulsory' content='Make sure you fill in all the fields' size='small' />}
+						{allFilled === false && (
+							<Message
+								error
+								header='All fields compulsory'
+								content='Make sure you fill in all the fields'
+								size='small'
+							/>
+						)}
 					</Segment>
 					<Message>
 						Already have an account? <Link to='/'>Login</Link>
