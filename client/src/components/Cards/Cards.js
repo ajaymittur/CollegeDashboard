36a6087b191c9c2d9bae1e6021c275aec7f54d52
akshaymittur <akshaymittur@gmail.com>
2019-10-08 16:00:06 +0530

    Classes converted to functions, and hooks introduced, bug check done later

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,60 +1,50 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Card } from "semantic-ui-react"
 import { VictoryBar, VictoryChart, VictoryAxis } from 'victory'
 import './Cards.css'
 
-class Cards extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			name: this.props.name,
-			subject: this.props.subject,
-			attendance: this.props.attendance,
-			marks: this.props.marks,
-			credits: this.props.credits,
-			data: [
-				{ x: "Attendance", y: this.props.attendance },
-				{ x: "Marks", y: this.props.marks }
-			]
-		}
-	}
+function Cards (props) {
+	const [ graph, setGraph ] = useState({
+		data: [
+			{ x: "Attendance", y: props.attendance },
+			{ x: "Marks", y: props.marks }
+		]
+	}) 
 
-	render(){
-		const { name, subject, attendance, marks, credits } = this.state
-		return (
-			<div>
-	  			<Card className='grow' raised>
-					<VictoryChart 
-						domainPadding={100} 
-						domain={{y: [0, 60]}}>
+	const { name, subject, attendance, marks, credits } = props
 
-						<VictoryAxis tickValues={
-							["Attendance", "Marks"]}
-          				/>
-						<VictoryAxis dependentAxis
-            				tickFormat={100} 
-            				/>
-						<VictoryBar
-					    	style={{ data: { fill: "#f5a153" } }}
-					    	data={this.state.data}
-					    	barRatio={0.7}
-					    	labels={({datum}) => datum.y}
-					    	/>
-					</VictoryChart>
-				    <Card.Content>
-				      <Card.Header>{`${ subject }`}</Card.Header>
-				      <Card.Meta>
-				        <span>{`${ credits } Credits`}</span>
-				      </Card.Meta>
-				      <Card.Description>
-				        {`${ name } has ${ attendance }% attendance and ${ marks }/100 marks in ${ subject }`}
-				      </Card.Description>
-				    </Card.Content>
-				</Card>
-			</div>
-		)
-	}
-	
+	return (
+		<div>
+  			<Card className='grow' raised>
+				<VictoryChart 
+					domainPadding={100} 
+					domain={{y: [0, 60]}}>
+
+					<VictoryAxis tickValues={
+						["Attendance", "Marks"]}
+      				/>
+					<VictoryAxis dependentAxis
+        				tickFormat={100} 
+        				/>
+					<VictoryBar
+				    	style={{ data: { fill: "#f5a153" } }}
+				    	data={graph.data}
+				    	barRatio={0.7}
+				    	labels={({datum}) => datum.y}
+				    	/>
+				</VictoryChart>
+			    <Card.Content>
+			      <Card.Header>{`${ subject }`}</Card.Header>
+			      <Card.Meta>
+			        <span>{`${ credits } Credits`}</span>
+			      </Card.Meta>
+			      <Card.Description>
+			        {`${ name } has ${ attendance }% attendance and ${ marks }/100 marks in ${ subject }`}
+			      </Card.Description>
+			    </Card.Content>
+			</Card>
+		</div>
+	)
 }
 
 export default Cards
--- client/src/components/InfoCards.js
+++ client/src/components/InfoCards.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Card, Image } from "semantic-ui-react"
 import { VictoryPie } from 'victory'
 import Cards from './Cards/Cards'
@@ -6,20 +6,7 @@ import Cards from './Cards/Cards'
 /*Note: 1) All Data will be fetched using axios and will replace the static data.
 	  	2) All formatting/Styling/Height/Width/Line Breaking will be done later.*/
 
-class InfoCards extends React.Component {
-	/*fetch('https://localhost:4000/:usn', {
-		method: 'post',
-		headers: {'Content-Type': 'application/json'},
-		body: JSON.stringify({
-		usn: this.props.usn
-			})
-		})
-	.then(response => response.json())
-	.then(res => {
-		this.setState(name: res.name)
-		this.setState(cgpa: res.cgpa)
-	})
-	*/
+function InfoCards() {
 	/*const javaData =[
 		{ x: "Attendance", y: 85 },
 		{ x: "Marks", y: 80 }
@@ -34,20 +21,18 @@ class InfoCards extends React.Component {
 		{x: "MP", y: 80},
 		{x: "PW", y: 80}
 	]*/
-	constructor() {
-		super()
-		this.state = {
-			name: '',
-			cgpa: '',
-			subject: [],
-			attendance: [],
-			marks: [],
-			credits: [],
-			data: []
-		}
-	}
 
-	/*componentDidMount(props){
+	const [studentData, setStudentData ] = useState({
+		name: '',
+		cgpa: '',
+		subjects: [],
+		attendance: [],
+		marks: [],
+		credits: [],
+		data: []
+	})
+
+	/*useEffect(() => {
  		fetch('https://localhost:4000/:usn', {
 		method: 'post',
 		headers: {'Content-Type': 'application/json'},
@@ -57,62 +42,61 @@ class InfoCards extends React.Component {
 		})
 		.then(response => response.json())
 		.then(res => {
-			this.setState({name: res[0].name})
-			this.setState({cgpa: res[0].cgpa})
-			this.setState({subjects : [...res[0].subjects]})
-			this.setState({attendance : [...res[0].attendance]})
-			this.setState({marks : [...res[0].marks]})
-			this.setState({credits : [...res[0].credits]})
+			setStudentData({...StudentData, StudentData {
+			name: res[0].name,
+			cgpa: res[0].cgpa,
+			subjects : [...res[0].subjects],
+			attendance : [...res[0].attendance],
+			marks : [...res[0].marks],
+			credits : [...res[0].credits]
+			}}
+			)
 			for (let i = 0; i < this.state.subjects.len: ++i) {
-				this.setState({data : [...this.state.data, { 
-					x: this.state.subjects[i], 
-					y: this.state.marks[i] }
+				setStudentData({...StudentData,data : [...StudentData.data], { 
+					x: studentData.subjects[i], 
+					y: studentData.marks[i] }
 					]
 				})
 			}
 		})
-	}*/
-
-	render(){
-		const { name, cgpa } = this.state
-		return (
-			<div>
-				<Card centered >
-					<Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-					<Card.Content>
-						<Card.Header>{`${ name }`}</Card.Header>
-						<Card.Meta>
-							<span>Student</span>
-						</Card.Meta>
-						<Card.Description>
-							{`${ name } is a student with ${ cgpa } CGPA.`}
-						</Card.Description>
-					</Card.Content>
-				</Card>
-				<Card.Group centered itemsPerRow={4}>
-					{
-						this.state.subjects.map((sub, i) => {
-							return (
-								<Cards 
-									key={i} 
-									name={name} 
-									subject={this.state.subjects[i]} 
-									attendance={this.state.attendance[i]} 
-									marks={this.state.marks[i]} 
-									credits={this.state.credits[i]} 
-								/>
-							)
-						})
-					}
-				</Card.Group>
-				<VictoryPie 
-				data={this.state.data} 
-				colorScale="qualitative" 
-				height={200} />
-			</div>
-		)
-
-	}
+	})*/
+	const { name, cgpa, subjects, attendance, marks, credits, data } = studentData
+	return (
+		<div>
+			<Card centered >
+				<Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+				<Card.Content>
+					<Card.Header>{`${ name }`}</Card.Header>
+					<Card.Meta>
+						<span>Student</span>
+					</Card.Meta>
+					<Card.Description>
+						{`${ name } is a student with ${ cgpa } CGPA.`}
+					</Card.Description>
+				</Card.Content>
+			</Card>
+			<Card.Group centered itemsPerRow={4}>
+				{
+					this.state.subjects.map((sub, i) => {
+						return (
+							<Cards 
+								key={i} 
+								name={name} 
+								subject={subjects[i]} 
+								attendance={attendance[i]} 
+								marks={marks[i]} 
+								credits={credits[i]} 
+							/>
+						)
+					})
+				}
+			</Card.Group>
+			<VictoryPie 
+			data={data} 
+			colorScale="qualitative" 
+			height={200} />
+		</div>
+	)
 }
 
 export default InfoCards
