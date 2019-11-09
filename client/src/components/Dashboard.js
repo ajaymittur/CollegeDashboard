d2713fbb79288494a5e5eae419dd8538cd08da3a
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-11-09 14:29:48 +0530

    Fixed stuck in loading screen, reduced header size

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,18 +6,88 @@ import Overview from "./Overview";
 import Scores from "./Scores";
 import Attendance from "./Attendance";
 import UserInfoCard from "./UserInfoCard";
+import NotFound from "./NotFound";
 import axios from "axios";
+
+// Check this
+const particleParams = {
+	fps_limit: 28,
+	particles: {
+		number: {
+			value: 200,
+			density: {
+				enable: false
+			}
+		},
+		line_linked: {
+			enable: true,
+			distance: 30,
+			opacity: 0.4
+		},
+		move: {
+			speed: 1
+		},
+		opacity: {
+			anim: {
+				enable: true,
+				opacity_min: 0.05,
+				speed: 2,
+				sync: false
+			},
+			value: 0.4
+		}
+	},
+	polygon: {
+		enable: true,
+		scale: 0.5,
+		type: "inline",
+		move: {
+			radius: 10
+		},
+		url: "/deer.svg",
+		inline: {
+			arrangement: "equidistant"
+		},
+		draw: {
+			enable: true,
+			stroke: {
+				color: "rgba(255, 255, 255, .2)"
+			}
+		}
+	},
+	retina_detect: false,
+	interactivity: {
+		events: {
+			onhover: {
+				enable: true,
+				mode: "bubble"
+			}
+		},
+		modes: {
+			bubble: {
+				size: 6,
+				distance: 40
+			}
+		}
+	}
+};
 
 function Dashboard() {
 	const [studentData, setStudentData] = useState({});
 	const [didFetchData, setDidFetchData] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(true);
 	const [activeItem, setActiveItem] = useState("Home");
 
 	useEffect(() => {
 		const fetchData = async () => {
-			const res = await axios.get("http://localhost:4000/student/getData");
-			setStudentData(res.data.userData);
-			setDidFetchData(true);
+			try {
+				const res = await axios.get("http://localhost:4000/student/getData");
+				setStudentData(res.data.userData);
+				setIsLoggedIn(res.data.isSuccess);
+				setDidFetchData(true);
+			} catch (err) {
+				setIsLoggedIn(false);
+			}
 		};
 
 		fetchData();
@@ -31,69 +101,8 @@ function Dashboard() {
 	};
 
 	const component = setComponent(activeItem);
-	const particleParams = {
-		fps_limit: 28,
-		particles: {
-			number: {
-				value: 200,
-				density: {
-					enable: false
-				}
-			},
-			line_linked: {
-				enable: true,
-				distance: 30,
-				opacity: 0.4
-			},
-			move: {
-				speed: 1
-			},
-			opacity: {
-				anim: {
-					enable: true,
-					opacity_min: 0.05,
-					speed: 2,
-					sync: false
-				},
-				value: 0.4
-			}
-		},
-		polygon: {
-			enable: true,
-			scale: 0.5,
-			type: "inline",
-			move: {
-				radius: 10
-			},
-			url: "/deer.svg",
-			inline: {
-				arrangement: "equidistant"
-			},
-			draw: {
-				enable: true,
-				stroke: {
-					color: "rgba(255, 255, 255, .2)"
-				}
-			}
-		},
-		retina_detect: false,
-		interactivity: {
-			events: {
-				onhover: {
-					enable: true,
-					mode: "bubble"
-				}
-			},
-			modes: {
-				bubble: {
-					size: 6,
-					distance: 40
-				}
-			}
-		}
-	};
 
-	if (didFetchData === false)
+	if (!didFetchData && isLoggedIn)
 		return (
 			<div>
 				<Dimmer active>
@@ -101,11 +110,12 @@ function Dashboard() {
 				</Dimmer>
 			</div>
 		);
-	else
+	else if (!isLoggedIn) {
+		return <NotFound />;
+	} else
 		return (
 			<div>
 				<Navbar name={studentData.fullname} activeItem={activeItem} setActiveItem={setActiveItem} />
-
 				{component}
 			</div>
 		);
--- client/src/components/Scores.js
+++ client/src/components/Scores.js
@@ -3,28 +3,23 @@ import { Card, Header, Grid } from "semantic-ui-react";
 import { VictoryPie } from "victory";
 
 function Scores({ studentData }) {
-  const { fullname, subjects, attendance, marks, credits } = studentData;
+	const { fullname, subjects, attendance, marks, credits } = studentData;
 
-  let chartData = [];
-  let i = 0;
-  for (let mark in marks) {
-    chartData.push({ x: Object.keys(marks)[i], y: mark });
-    ++i;
-  }
+	let chartData = [];
+	let i = 0;
+	for (let mark in marks) {
+		chartData.push({ x: Object.keys(marks)[i], y: mark });
+		++i;
+	}
 
-  return (
-    <div>
-      <Header
-        size="huge"
-        textAlign="center"
-        color="orange"
-        style={{ marginTop: "5%", fontSize: "400%" }}
-      >
-        Marks
-      </Header>
-      <VictoryPie data={chartData} colorScale="qualitative" height={300} />
-    </div>
-  );
+	return (
+		<div>
+			<Header size='large' textAlign='center' color='orange' style={{ marginTop: "5%" }}>
+				Marks
+			</Header>
+			<VictoryPie data={chartData} colorScale='qualitative' height={300} />
+		</div>
+	);
 }
 
 export default Scores;
