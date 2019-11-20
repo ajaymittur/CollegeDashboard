4f62c3587c19f379824cd0f05c09afc4e7088354
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-11-20 22:33:00 +0530

    router path bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -10,60 +10,60 @@ import LandingPage from "./components/HomePage/LandingPage";
 import Particles from "react-particles-js";
 
 const particleParams = {
-  particles: {
-    number: {
-      value: 140,
-      density: {
-        enable: false,
-        value_area: 800
-      }
-    },
-    color: {
-      value: ["#FFA500"]
-    },
-    line_linked: {
-      color: "#FFA500",
-      opacity: 1
-    }
-  }
+	particles: {
+		number: {
+			value: 140,
+			density: {
+				enable: false,
+				value_area: 800
+			}
+		},
+		color: {
+			value: ["#FFA500"]
+		},
+		line_linked: {
+			color: "#FFA500",
+			opacity: 1
+		}
+	}
 };
 
 function App() {
-  return (
-    <Router>
-      <Particles params={particleParams} height={document.body.scrollHeight} />
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%"
-        }}
-      >
-        <Switch>
-          <Route path="/" exact>
-            <LandingPage />
-          </Route>
-          <Route path="/login" exact>
-            <LoginForm />
-          </Route>
-          <Route path="/reset">
-            <ResetPassForm />
-          </Route>
-          <Route path="/signup">
-            <SignUpForm />
-          </Route>
-          <Route path="/student/dashboard">
-            <Dashboard />
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<Particles params={particleParams} height={document.body.scrollHeight} />
+			<div
+				style={{
+					position: "absolute",
+					top: 0,
+					left: 0,
+					width: "100%",
+					height: "100%"
+				}}
+			>
+				<Switch>
+					<Route path='/' exact>
+						<LandingPage />
+					</Route>
+					<Route path='/login'>
+						<LoginForm />
+					</Route>
+					<Route path='/reset'>
+						<ResetPassForm />
+					</Route>
+					<Route path='/signup'>
+						<SignUpForm />
+					</Route>
+					<Route path='/student/dashboard'>
+						<Dashboard />
+					</Route>
+					<Route>
+						<NotFound />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
