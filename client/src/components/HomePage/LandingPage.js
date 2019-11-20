9be93b3ceca8e35c98f1304f2141919cc42a0338
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-11-20 21:29:00 +0530

    removed unused declarations

--- client/src/components/HomePage/LandingPage.js
+++ client/src/components/HomePage/LandingPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Menu, Dropdown } from "semantic-ui-react";
 import { withRouter } from "react-router-dom";
 import LandingNav from "./LandingNav";
 import Type from "./Type";
