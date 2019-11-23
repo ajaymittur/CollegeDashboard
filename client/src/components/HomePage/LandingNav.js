ec863c65da774f9089800a52bcc25613abb10036
akshaymittur <akshaymittur@gmail.com>
2019-11-23 23:18:17 +0530

    Added Button Animation

--- client/src/components/HomePage/LandingNav.js
+++ client/src/components/HomePage/LandingNav.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { Menu, Button } from "semantic-ui-react";
+import { Menu, Button, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 function LandingNav() {
-	return (
-		<Menu>
-			<Menu.Menu position='right'>
-				<Menu.Item>
-					<Button size='large' as={Link} to='/signup'>
-						Sign Up
-					</Button>
-				</Menu.Item>
+  return (
+    <Menu>
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Button size="large" as={Link} to="/signup" animated="fade">
+            <Button.Content visible>Sign Up</Button.Content>
+            <Button.Content hidden>Join Us!</Button.Content>
+          </Button>
+        </Menu.Item>
 
-				<Menu.Item>
-					<Button size='large' color='orange' as={Link} to='/login'>
-						Log in
-					</Button>
-				</Menu.Item>
-			</Menu.Menu>
-		</Menu>
-	);
+        <Menu.Item>
+          <Button size="large" color="orange" as={Link} to="/login" animated>
+            <Button.Content visible>Login</Button.Content>
+            <Button.Content hidden>
+              <Icon name="arrow right" />
+            </Button.Content>
+          </Button>
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
 }
 
 export default LandingNav;
