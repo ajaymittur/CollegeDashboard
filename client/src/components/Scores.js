e73cebc75949066d0366cc94496ffb6d5083b1e0
akshaymittur <akshaymittur@gmail.com>
2019-11-23 20:44:06 +0530

    Added Line graph, fixed pie chart bug, styled graphs

--- client/src/components/Scores.js
+++ client/src/components/Scores.js
@@ -1,26 +1,105 @@
 import React from "react";
 import { Header } from "semantic-ui-react";
-import { VictoryPie } from "victory";
+import { VictoryPie, VictoryChart, VictoryLine, VictoryTheme } from "victory";
 
 function Scores({ studentData }) {
-	const { marks } = studentData;
+  const { marks } = studentData;
 
-	let chartData = [];
-	let i = 0;
-	for (let mark in marks) {
-		console.log(mark);
-		chartData.push({ x: Object.keys(marks)[i], y: mark });
-		++i;
-	}
+  let chartData = [];
+  let i = 0;
+  for (let mark in marks) {
+    chartData.push({ y: Object.values(marks)[i], x: mark });
+    ++i;
+  }
 
-	return (
-		<div>
-			<Header size='large' textAlign='center' color='orange' style={{ marginTop: "5%" }}>
-				Marks
-			</Header>
-			<VictoryPie data={chartData} colorScale='qualitative' height={300} />
-		</div>
-	);
+  return (
+    <div>
+      <Header
+        as="h1"
+        textAlign="center"
+        color="orange"
+        style={{ marginTop: "3%" }}
+      >
+        Marks
+      </Header>
+      <div
+        style={{
+          width: "50%",
+          float: "left",
+          marginTop: "1%"
+        }}
+      >
+        <VictoryPie
+          data={chartData}
+          colorScale="qualitative"
+          height={300}
+          style={{ labels: { fontSize: "17px" } }}
+          events={[
+            {
+              target: "data",
+              eventHandlers: {
+                onClick: () => {
+                  return [
+                    {
+                      target: "data",
+                      mutation: ({ style }) => {
+                        return style.fill === "#c43a31"
+                          ? null
+                          : { style: { fill: "#c43a31" } };
+                      }
+                    },
+                    {
+                      target: "labels"
+                    }
+                  ];
+                }
+              }
+            }
+          ]}
+        />
+      </div>
+      <div
+        style={{
+          width: "50%",
+          float: "right",
+          marginTop: "1%"
+        }}
+      >
+        <VictoryChart
+          theme={VictoryTheme.material}
+          domain={{ y: [0, 100] }}
+          height={300}
+          width={400}
+        >
+          <VictoryLine
+            style={{ data: { fill: "#ff9b21" } }}
+            alignment="start"
+            data={chartData}
+            events={[
+              {
+                target: "data",
+                eventHandlers: {
+                  onClick: () => {
+                    return [
+                      {
+                        target: "data",
+                        mutation: props => {
+                          const fill = props.style && props.style.fill;
+                          return fill === "black"
+                            ? null
+                            : { style: { fill: "black" } };
+                        }
+                      }
+                    ];
+                  }
+                }
+              }
+            ]}
+          />
+        </VictoryChart>
+      </div>
+    </div>
+  );
 }
 
 export default Scores;
